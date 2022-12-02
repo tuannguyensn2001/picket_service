fc3467e8c365a6b0a2d527a9fdc46c95d2e5221c
tuannguyensn2001 <devpro2001@gmail.com>
2022-12-02 10:56:51 +0700

    update

---
+++ src/features/test/repository/get_test_multiple_choice_by_test_id.go
@@ -0,0 +1,10 @@
+package test_repository
+
+import (
+	"context"
+	"myclass_service/src/entities"
+)
+
+func (r *repo) FindTestMultipleChoiceByTestId(ctx context.Context, testId int) (*entities.TestMultipleChoice, error) {
+	return nil, nil
+}
---
+++ src/features/test/repository/save_test_multiple_choice.go
@@ -0,0 +1,21 @@
+package test_repository
+
+import (
+	"context"
+	"myclass_service/src/entities"
+)
+
+func (r *repo) SaveTestMultipleChoice(ctx context.Context, entity *entities.TestMultipleChoice) error {
+	model := multipleChoice{
+		Id:        entity.Id,
+		FilePath:  entity.FilePath,
+		Score:     entity.Score,
+		CreatedAt: entity.CreatedAt,
+		UpdatedAt: entity.UpdatedAt,
+	}
+	db := r.GetDB(ctx)
+	if err := db.WithContext(ctx).Save(&model).Error; err != nil {
+		return err
+	}
+	return nil
+}
