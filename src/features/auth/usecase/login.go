506c7c3e4b36971c613dd1d923c0b68b6c28d28c
Tuan Nguyen <devpro2001@gmail.com>
2022-12-06 22:04:49 +0700

    update

--- src/features/auth/usecase/login.go
+++ src/features/auth/usecase/login.go
@@ -10,26 +10,37 @@ import (
 )
 
 func (u *usecase) Login(ctx context.Context, input auth_struct.LoginInput) (*auth_struct.LoginOutput, error) {
+	ctx, span := tracer.Start(ctx, "start login")
+	defer span.End()
+
+	ctx, span = tracer.Start(ctx, "validate")
 	validate := validator.New()
 	err := validate.Struct(input)
+	span.End()
 	if err != nil {
 		zap.S().Error(err)
 		return nil, errpkg.Auth.LoginFailed
 	}
 
+	ctx, span = tracer.Start(ctx, "get user by email")
 	user, err := u.userUsecase.GetByEmail(ctx, input.Email)
+	span.End()
 	if err != nil {
 		zap.S().Error(err)
 		return nil, errpkg.Auth.LoginFailed
 	}
 
+	ctx, span = tracer.Start(ctx, "compare password")
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
+	span.End()
 	if err != nil {
 		zap.S().Error(err)
 		return nil, errpkg.Auth.LoginFailed
 	}
 
+	ctx, span = tracer.Start(ctx, "generat jwt")
 	token, err := u.GenerateToken(ctx, *user)
+	span.End()
 	if err != nil {
 		zap.S().Error(err)
 		return nil, errpkg.Auth.LoginFailed
